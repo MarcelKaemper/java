129e16544b198733e5c1d79b0a698783cafcfa0d
Marcel <marcelkaemper862@gmail.com>
2019-05-18 15:51:46 +0200

    Added possible operators

--- conditions.java
+++ conditions.java
@@ -2,6 +2,8 @@ public class conditions {
     public static void main(String args[]){
         int num = 5;
 
+        // > < >= <= == !=
+        // && ||
         if(num == 5){
             System.out.println("num is 5");
         }else if(num > 5){
