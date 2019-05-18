35953d16eafc0113ed12a6676234a77822f6c58e
Marcel <marcelkaemper862@gmail.com>
2019-05-18 15:49:27 +0200

    Added condition example

---
+++ conditions.java
@@ -0,0 +1,13 @@
+public class conditions {
+    public static void main(String args[]){
+        int num = 5;
+
+        if(num == 5){
+            System.out.println("num is 5");
+        }else if(num > 5){
+            System.out.println("num greater 5");
+        }else{
+            System.out.println("num less 5");
+        }
+    }
+}
