81b9675e0cfc4483c80d7692c3279ab30dd32fed
Marcel <marcelkaemper862@gmail.com>
2019-05-18 16:02:18 +0200

    Added switch example

---
+++ switchStatement.java
@@ -0,0 +1,18 @@
+public class switchStatement {
+    public static void main(String args[]){
+        int age = 6;
+
+        switch(age){
+            case 1:
+                break;
+            case 2:
+                break;
+            case 25:
+                System.out.println("25");
+                break;
+            default:
+                System.out.println("Something else");
+                break;
+        }
+    }
+}
