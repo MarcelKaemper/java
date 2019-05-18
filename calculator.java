f3650ac7861c9de2c6bfa55f927be547a90c0d66
Marcel <marcelkaemper862@gmail.com>
2019-05-18 12:53:19 +0200

    Added basic calculation functions

--- calculator.java
+++ calculator.java
@@ -3,6 +3,12 @@ import java.util.Scanner;
 public class calculator {
     public static void main(String args[]){
         Scanner scr = new Scanner(System.in);
-
+        double num1,num2,res;
+        System.out.println("Enter number 1:");
+        num1 = scr.nextDouble();
+        System.out.println("Enter number 2:");
+        num2 = scr.nextDouble();
+        res = num1+num2;
+        System.out.println(res);
     }
 }
