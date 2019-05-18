720832694643130232179cc37c83a7deb9db996b
Marcel <marcelkaemper862@gmail.com>
2019-05-18 12:47:47 +0200

    Added variables example

---
+++ variables.java
@@ -0,0 +1,11 @@
+public class variables {
+    public static void main(String args[]){
+        double pi;
+        pi = 3.1415;
+
+        String str = "Hello World";
+
+        System.out.println("Pi: "+pi);
+        System.out.println(str);
+    }
+}
